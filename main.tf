9859be23182e1519de34ff596bd9cf49f1306ccf
MeenalJy <meenaljyotikaa@gmail.com>
2023-09-20 17:58:20 +0530

    Added Terraform configuration file

--- main.tf
+++ main.tf
@@ -1,9 +1,18 @@
-resource "null_resource" "fortune_cookie_generator" {
+# Use local-exec to select a random line from the file and store it in an output
+
+resource "null_resource" "select_random_fortune" {
   triggers = {
-    always_run = "${timestamp()}"
+    filename = "fortune_cookies.txt"
   }
 
   provisioner "local-exec" {
-    command = "sh -c 'echo \"Your fortune cookie message: $(shuf -n 1 fortune_cookies.txt)\" > show_fortune.txt'"
+    command = "shuf -n 1 fortune_cookies.txt > show_fortune.txt"
   }
 }
+
+# Define an output to display the random fortune cookie message
+
+output "my_fortune" {
+  value = file("show_fortune.txt")
+}
+
